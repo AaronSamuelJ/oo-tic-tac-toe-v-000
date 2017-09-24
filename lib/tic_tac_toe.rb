c4d5b99fb2cfbe8507475cadabc8420780e0084e
Aaron Samuel <aasamuelj@gmail.com>
2017-09-24 04:46:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,7 +52,7 @@ class TicTacToe
   def won?
     WIN_COMBINATIONS.find do |combo|
       binding.pry
-      position_taken?(@board[combo[0]]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+      position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
     end
   end
 end
